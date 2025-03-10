5108df5df0ee15fee2f65c6597c8b35a1252a86e
turing <phtalmeida18@ua.pt>
2025-03-10 23:18:50 +0000

    Ficheiros criados -> FEITO

---
+++ ConversorUnidades/src/main/java/com/mycompany/conversorunidades/ConversorUnidades.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversorunidades;
+
+/**
+ *
+ * @author Pedro Almeida
+ */
+public class ConversorUnidades {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
