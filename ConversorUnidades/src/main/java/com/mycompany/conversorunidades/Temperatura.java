efb6b39dbf59c005ccf166726149a31d73526cc5
turing <phtalmeida18@ua.pt>
2025-03-17 17:50:32 +0000

    Temperatura

--- ConversorUnidades/src/main/java/com/mycompany/conversorunidades/Temperatura.java
+++ ConversorUnidades/src/main/java/com/mycompany/conversorunidades/Temperatura.java
@@ -9,5 +9,22 @@ package com.mycompany.conversorunidades;
  * @author Pedro Almeida
  */
 public class Temperatura { 
+    // Conversão de Temperatura
+    public static double converterTemperatura(double valor, String unidadeOrigem, String unidadeDestino) {
+        if (unidadeOrigem.equals("C") && unidadeDestino.equals("F")) {
+            return (valor * 9/5) + 32;
+        } else if (unidadeOrigem.equals("C") && unidadeDestino.equals("K")) {
+            return valor + 273.15;
+        } else if (unidadeOrigem.equals("F") && unidadeDestino.equals("C")) {
+            return (valor - 32) * 5/9;
+        } else if (unidadeOrigem.equals("F") && unidadeDestino.equals("K")) {
+            return (valor - 32) * 5/9 + 273.15;
+        } else if (unidadeOrigem.equals("K") && unidadeDestino.equals("C")) {
+            return valor - 273.15;
+        } else if (unidadeOrigem.equals("K") && unidadeDestino.equals("F")) {
+            return (valor - 273.15) * 9/5 + 32;
+        }
+        return valor; // Se as unidades forem iguais
+    }
     
 }
