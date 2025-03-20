f9b0832d7610a9a912db7ecb6f6ffae6dc65ce15
turing <phtalmeida18@ua.pt>
2025-03-20 10:45:44 +0000

    Ficheiros retificados

--- ConversorUnidades/src/main/java/com/mycompany/conversorunidades/Fifo.java
+++ ConversorUnidades/src/main/java/com/mycompany/conversorunidades/Fifo.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.conversorunidades;
-
+import java.util.LinkedList;
 /**
  *
  * @author Pedro Almeida
  */
-public class Fifo {
-    
+
+public class Fifo{
+    private LinkedList buffer = new LinkedList<>();
+
+    public void adicionarElemento(T elemento) {
+        buffer.addLast(elemento);
+    }
+
+    public void removerElemento() {
+        return buffer.pollFirst(); // Remove o primeiro elemento (FIFO)
+    }
+
+    public void listarElementos() {
+        System.out.println(buffer);
+    }
 }
+    
+
